cf266d7f7083cebdb14a21816f05a44775889b67
PaoloTorregroza <paolodydtorregrosa@gmail.com>
2020-07-09 03:44:37 -0500

    feat: add job schema and interface

--- src/models/Jobs.ts
+++ src/models/Jobs.ts
@@ -1,5 +1,35 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
+
+export interface IJob extends Document {
+    title: string;
+    description: string;
+    date: Date;    
+}
+
 const JobsSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    employer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    proposals: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User'
+    }
+},{
+    timestamps: true
 });
-export default model("Jobs", JobsSchema);
+
+export default model<IJob>("Jobs", JobsSchema);
