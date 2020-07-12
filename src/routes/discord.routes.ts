20255e91431f0c6f8293c9eea0c8309f10fa6fc1
Fedeya <elfedeminaya@gmail.com>
2020-07-12 01:20:55 -0300

    feat: used controllers in the router and used discord router

--- src/routes/discord.routes.ts
+++ src/routes/discord.routes.ts
@@ -1,4 +1,13 @@
 import { Router } from 'express';
+
+import { handlerExceptionRoute } from '../error';
+import * as discord from '../controllers/discord.controller';
+
 const router = Router();
+
+router
+  .route('/setting/:name')
+  .get(handlerExceptionRoute(discord.getSetting))
+  .put(handlerExceptionRoute(discord.updateOrCreateSetting));
 
 export default router;
