f4ae7f54ee62e9f7f859deae8c19ac9c365a91f6
Fedeya <elfedeminaya@gmail.com>
2020-07-12 01:18:58 -0300

    feat: created schema for setting and discord users

--- src/models/Discord.ts
+++ src/models/Discord.ts
@@ -1,7 +1,39 @@
 import { Schema, model } from 'mongoose';
 
-const DiscordSchema = new Schema({
-
+const SettingSchema = new Schema({
+  name: {
+    type: String
+  },
+  value: {
+    type: String
+  }
 });
 
-export default model("Discord", DiscordSchema);
+const DiscordUserSchema = new Schema({
+  type: {
+    type: String
+  },
+  user_id: {
+    type: String,
+    required: true
+  },
+  reason: {
+    type: String
+  },
+  moderator_id: {
+    type: String
+  },
+  expiration_date: {
+    type: Date
+  },
+  creation_date: {
+    type: Date,
+    default: new Date()
+  },
+  revoked: {
+    type: Boolean
+  }
+});
+
+export const Setting = model('Setting', SettingSchema);
+export const DiscordUser = model('DiscordUser', DiscordUserSchema);
