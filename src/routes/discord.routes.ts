5b951111774abbf44903f0d99a855914c1311747
Fedeya <elfedeminaya@gmail.com>
2020-07-12 01:52:06 -0300

    feat: used controllers for moderations in the router

--- src/routes/discord.routes.ts
+++ src/routes/discord.routes.ts
@@ -10,4 +10,17 @@ router
   .get(handlerExceptionRoute(discord.getSetting))
   .put(handlerExceptionRoute(discord.updateOrCreateSetting));
 
+router
+  .route('/moderation/:user_id')
+  .get(handlerExceptionRoute(discord.getUserModerations))
+  .post(handlerExceptionRoute(discord.createModerationUser));
+
+router
+  .route('/moderation/:user_id/:type')
+  .get(handlerExceptionRoute(discord.getUserModerationsWithType));
+
+router
+  .route('/moderation/:id')
+  .patch(handlerExceptionRoute(discord.revokeModeration));
+
 export default router;
