2ebd7f2910dc6cf0ce5b8c7733cfb5589b3d627e
Antony Giomar <antonygiomarx@gmail.com>
2020-07-21 15:28:32 -0600

    feat(__tests__): tests were added for projects and for tasks routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
 import { Router } from 'express';
@@ -12,35 +11,6 @@ import indexRoutes from './index.routes';
 import miscRoutes from './misc.routes';
 import newsRoutes from './new.routes';
 import githubRoutes from './github.routes';
-
-const routes = Router();
-
-routes.use('/', indexRoutes);
-routes.use('/bot', discordRoutes);
-routes.use('/misc', miscRoutes);
-routes.use('/tasks', taskRoutes);
-routes.use('/projects', projectsRoutes);
-routes.use('/users', userRoutes);
-routes.use('/jobs', jobRoutes);
-routes.use('/news', newsRoutes);
-routes.use('/github', githubRoutes);
-
-export default routes;
-=======
-// Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
-
-import { Router } from 'express';
-
-import taskRoutes from './tasks.routes';
-import projectsRoutes from './projects.routes';
-import userRoutes from './users.routes';
-import jobRoutes from './jobs.routes';
-import discordRoutes from './discord.routes';
-import indexRoutes from './index.routes';
-import miscRoutes from './misc.routes';
-import newsRoutes from './new.routes';
-import githubRoutes from './github.routes';
-
 
 const routes = Router();
 
@@ -57,4 +27,3 @@ routes.use('/news', newsRoutes);
 routes.use('/github', githubRoutes);
 
 export default routes;
->>>>>>> Stashed changes
