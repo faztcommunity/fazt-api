3e50b334fbbf6c62956942fa554c70230b021329
Federico Minaya <elfedeminaya@gmail.com>
2020-07-22 23:55:19 -0300

    fix(uploads): fixed don't show images for api-key (#82)

--- src/middlewares/api-key.middleware.ts
+++ src/middlewares/api-key.middleware.ts
@@ -4,10 +4,12 @@ import { ErrorHandler } from '../error';
 import { UNAUTHORIZED } from 'http-status-codes';
 import { API_KEY } from '../config';
 
+const publicRoutes = ['api-docs', 'uploads'];
+
 const apiKeyMiddleware: Handler = (req, res, next) => {
   const apiKey = req.headers['api-key'];
   const url = req.url.split('/')[1];
-  if (url === 'api-docs') return next();
+  if (publicRoutes.includes(url)) return next();
 
   if (!apiKey || apiKey !== API_KEY)
     throw new ErrorHandler(UNAUTHORIZED, 'api key is required');
