7f0edec15f143a5feb7fd4a7f166506f8a32e2da
Fedeya <elfedeminaya@gmail.com>
2020-08-09 16:24:42 -0300

    refactor: linted with prettier

--- src/entities/skill-category.entity.ts
+++ src/entities/skill-category.entity.ts
@@ -1,22 +1,29 @@
-import { Column, Entity, PrimaryGeneratedColumn, Index, ManyToOne, JoinColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Index,
+  ManyToOne,
+  JoinColumn
+} from 'typeorm';
 import { SkillEntity } from './skill.entity';
 
 @Index('uk_skill_category', ['idSkill', 'idCategory'], { unique: true })
 @Entity('skill_category')
 export class SkillCategoryEntity {
-    @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
-    id: number;
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
 
-    @Column('integer', { name: 'id_skill' })
-    idSkill: number;
+  @Column('integer', { name: 'id_skill' })
+  idSkill: number;
 
-    @Column('integer', {name: 'id_category' })
-    idCategory: number;
+  @Column('integer', { name: 'id_category' })
+  idCategory: number;
 
-    @ManyToOne(() => SkillEntity, skill => skill.skillCategory, {
-        onDelete: 'RESTRICT',
-        onUpdate: 'RESTRICT'
-    })
-    @JoinColumn([{ name: 'id_skill', referencedColumnName: 'id' }])
-    skill: SkillEntity;
+  @ManyToOne(() => SkillEntity, skill => skill.skillCategory, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn([{ name: 'id_skill', referencedColumnName: 'id' }])
+  skill: SkillEntity;
 }
--- src/entities/team.entity.ts
+++ src/entities/team.entity.ts
@@ -1,21 +1,21 @@
 // Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 import { Column, Entity, PrimaryGeneratedColumn, Index } from 'typeorm';
 
-@Index("uk_name_team", ["nameTeam"], { unique: true })
+@Index('uk_name_team', ['nameTeam'], { unique: true })
 @Entity('team')
 export class TeamEntity {
-    @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
-    id: number;
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
 
-    @Column('character varying', { name: 'name_team', length: 45 })
-    nameTeam: string;
+  @Column('character varying', { name: 'name_team', length: 45 })
+  nameTeam: string;
 
-    @Column('text', { name: 'team_description' })
-    teamDescription: string;
+  @Column('text', { name: 'team_description' })
+  teamDescription: string;
 
-    @Column('integer', { name: 'team_capacity' })
-    teamCapacity: number;
+  @Column('integer', { name: 'team_capacity' })
+  teamCapacity: number;
 
-    @Column('character varying', { name: 'state_team', length: 20 })
-    stateTeam: string;
+  @Column('character varying', { name: 'state_team', length: 20 })
+  stateTeam: string;
 }
