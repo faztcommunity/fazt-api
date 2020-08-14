dedf2119df8bd21b77bd61ec398e0fc912e714f3
Fedeya <elfedeminaya@gmail.com>
2020-08-14 19:33:45 -0300

    feat(entities): added project-team-user entities

---
+++ src/entities/project-team.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Index,
+  ManyToOne,
+  JoinColumn
+} from 'typeorm';
+import { TeamEntity } from './team.entity';
+import { ProjectEntity } from './project.entity';
+
+@Index('uk_project_team', ['team', 'project'], { unique: true })
+@Entity('project_team')
+export class ProjectTeamEntity {
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
+
+  @Column({ name: 'state_project_team', type: 'varchar', length: 20 })
+  stateProjectTeam: string;
+
+  @ManyToOne(() => TeamEntity, team => team.projectTeam, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_team', referencedColumnName: 'id' })
+  team: TeamEntity;
+
+  @ManyToOne(() => ProjectEntity, project => project.projectTeam, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_project', referencedColumnName: 'id' })
+  project: ProjectEntity;
+}
---
+++ src/entities/project-user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  ManyToOne,
+  Index,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  OneToMany,
+  Column
+} from 'typeorm';
+import { ProjectEntity } from './project.entity';
+import { UserEntity } from './user.entity';
+import { TeamProjectEntity } from './team-project.entity';
+
+@Entity('project_user')
+@Index('uk_project_user', ['user', 'project'], { unique: true })
+export class ProjectUserEntity {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'integer' })
+  id: number;
+
+  @Column({ name: 'state_user_project', type: 'varchar', length: 20 })
+  stateUserProject: string;
+
+  @ManyToOne(() => ProjectEntity, project => project.projectUser, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_project', referencedColumnName: 'id' })
+  project: ProjectEntity;
+
+  @ManyToOne(() => UserEntity, user => user.projectUser, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_user', referencedColumnName: 'id' })
+  user: UserEntity;
+
+  @OneToMany(() => TeamProjectEntity, teamProject => teamProject.projectUser, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  teamProject: TeamProjectEntity[];
+}
