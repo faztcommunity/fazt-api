62de2e3922b02df47ce52a5755990582f4d94194
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-16 19:21:32 -0500

    chore: verify and create default roles

---
+++ src/common/roles/default.ts
@@ -0,0 +1,6 @@
+export enum Roles {
+  ADMIN = 'admin',
+  LEADER = 'lider',
+  COLLABORATOR = 'colaborador',
+  GUEST = 'invitado'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import app from './app';
 import { createConnection } from 'typeorm';
 
+import { createRoles } from './utils/create.roles';
+
 async function init() {
   try {
-    await createConnection();
+    const con = await createConnection();
     console.log('Postgres is Online');
+    createRoles(con);
+
   } catch (err) {
     console.log('Error Creating Postgres Connection:', err.message);
   }
