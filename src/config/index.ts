aca40a9926e247aeb5e99d78f192a32551e10587
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-16 23:08:31 -0500

    chore: add variables for admin default

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,7 @@ const ENV: NodeJS.ProcessEnv = process.env;
 
 export const PORT = ENV.PORT || 4000;
 export const JWT_SECRET = ENV.JWT_SECRET || 'secretword';
+
+export const ADMIN_EMAIL = ENV.ADMIN_EMAIL || 'admin@faztcommunity.dev';
+export const ADMIN_USERNAME = ENV.ADMIN_USERNAME || 'admin';
+export const ADMIN_PASSWORD = ENV.ADMIN_PASSWORD || 'admin';
