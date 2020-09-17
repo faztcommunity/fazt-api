c067976c3bbab1b1ec59617139120104ce4ad3e5
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-16 23:14:31 -0500

    chore: getUsers for rol admin

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { UserController } from '../controllers/user.controller';
 import { ErrorRouter } from '../error';
-import { auth } from '../middlewares/auth.middleware';
+import { auth, isAdmin } from '../middlewares/auth.middleware';
 
 const router = new ErrorRouter();
 
 router
   .route('/')
-  .get(UserController.getUsers)
+  .get(auth, isAdmin, UserController.getUsers)
   .post(UserController.createUser)
   .delete(auth, UserController.deleteUser)
   .put(auth, UserController.updateUserData);
